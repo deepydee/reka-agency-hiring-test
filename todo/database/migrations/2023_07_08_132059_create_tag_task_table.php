fdd3dcac0fc1998487b1fe062e694a08d2609a83
Vitaliy Semenov <synthex@bk.ru>
2023-07-08 19:56:51 +0600

    feat: add DB Schema

---
+++ todo/database/migrations/2023_07_08_132059_create_tag_task_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tag_task', function (Blueprint $table) {
+            $table->foreignId('tag_id')
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+            $table->foreignId('task_id')
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tag_task');
+    }
+};
